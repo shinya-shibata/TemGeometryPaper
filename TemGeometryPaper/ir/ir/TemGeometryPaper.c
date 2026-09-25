e5e6ed1505058bc3c36e00baf0ab12c6569e892a
shinya-shibata <142186186+shinya-shibata@users.noreply.github.com>
2026-09-26 08:48:20 +0900

    Add files via upload

---
+++ TemGeometryPaper/ir/ir/TemGeometryPaper.c
@@ -0,0 +1,256 @@
+// Lean compiler output
+// Module: TemGeometryPaper
+// Imports: public import Init public meta import Init public import Mathlib
+#include <lean/lean.h>
+#if defined(__clang__)
+#pragma clang diagnostic ignored "-Wunused-parameter"
+#pragma clang diagnostic ignored "-Wunused-label"
+#elif defined(__GNUC__) && !defined(__CLANG__)
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wunused-label"
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+#endif
+#ifdef __cplusplus
+extern "C" {
+#endif
+lean_object* lp_mathlib_Nat_cast___at___00Nat_cast___at___00Nat_cast___at___00Nat_cast___at___00__private_Mathlib_NumberTheory_ModularForms_EisensteinSeries_E2_Transform_0__EisensteinSeries_00_u03b4_spec__0_spec__0_spec__2_spec__3(lean_object*);
+lean_object* lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Basic_Real_Basic_4282658333____hygCtx___hyg_2_(lean_object*, lean_object*);
+lean_object* lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Basic_Real_Basic_1745762101____hygCtx___hyg_2_(lean_object*, lean_object*, lean_object*);
+lean_object* lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Basic_Real_Basic_2153608522____hygCtx___hyg_2_(lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_TemGeometryPaper_TEMPaper_observedInformation(lean_object*, lean_object*, lean_object*);
+static lean_once_cell_t lp_TemGeometryPaper_TEMPaper_nuEq22___closed__0_once = LEAN_ONCE_CELL_INITIALIZER;
+static lean_object* lp_TemGeometryPaper_TEMPaper_nuEq22___closed__0;
+static lean_once_cell_t lp_TemGeometryPaper_TEMPaper_nuEq22___closed__1_once = LEAN_ONCE_CELL_INITIALIZER;
+static lean_object* lp_TemGeometryPaper_TEMPaper_nuEq22___closed__1;
+LEAN_EXPORT lean_object* lp_TemGeometryPaper_TEMPaper_nuEq22(lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_TemGeometryPaper_TEMPaper_kappaEq23(lean_object*);
+LEAN_EXPORT lean_object* lp_TemGeometryPaper_TEMPaper_kappaEq23___boxed(lean_object*);
+LEAN_EXPORT lean_object* lp_TemGeometryPaper_TEMPaper_fisher1;
+static lean_once_cell_t lp_TemGeometryPaper_TEMPaper_GammaNormal1___closed__0_once = LEAN_ONCE_CELL_INITIALIZER;
+static lean_object* lp_TemGeometryPaper_TEMPaper_GammaNormal1___closed__0;
+static lean_once_cell_t lp_TemGeometryPaper_TEMPaper_GammaNormal1___closed__1_once = LEAN_ONCE_CELL_INITIALIZER;
+static lean_object* lp_TemGeometryPaper_TEMPaper_GammaNormal1___closed__1;
+LEAN_EXPORT lean_object* lp_TemGeometryPaper_TEMPaper_GammaNormal1;
+static lean_once_cell_t lp_TemGeometryPaper_TEMPaper_TNormal1___closed__0_once = LEAN_ONCE_CELL_INITIALIZER;
+static lean_object* lp_TemGeometryPaper_TEMPaper_TNormal1___closed__0;
+LEAN_EXPORT lean_object* lp_TemGeometryPaper_TEMPaper_TNormal1;
+static lean_once_cell_t lp_TemGeometryPaper___private_TemGeometryPaper_0__TEMPaper_gPart1___closed__0_once = LEAN_ONCE_CELL_INITIALIZER;
+static lean_object* lp_TemGeometryPaper___private_TemGeometryPaper_0__TEMPaper_gPart1___closed__0;
+static lean_once_cell_t lp_TemGeometryPaper___private_TemGeometryPaper_0__TEMPaper_gPart1___closed__1_once = LEAN_ONCE_CELL_INITIALIZER;
+static lean_object* lp_TemGeometryPaper___private_TemGeometryPaper_0__TEMPaper_gPart1___closed__1;
+LEAN_EXPORT lean_object* lp_TemGeometryPaper___private_TemGeometryPaper_0__TEMPaper_gPart1(lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_TemGeometryPaper_TEMPaper_observedInformation(lean_object* v_g_1_, lean_object* v_H_2_, lean_object* v_rN_3_){
+_start:
+{
+lean_object* v___f_4_; lean_object* v___f_5_; lean_object* v___f_6_; 
+v___f_4_ = lean_alloc_closure((void*)(lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Basic_Real_Basic_1745762101____hygCtx___hyg_2_), 3, 2);
+lean_closure_set(v___f_4_, 0, v_H_2_);
+lean_closure_set(v___f_4_, 1, v_rN_3_);
+v___f_5_ = lean_alloc_closure((void*)(lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Basic_Real_Basic_4282658333____hygCtx___hyg_2_), 2, 1);
+lean_closure_set(v___f_5_, 0, v___f_4_);
+v___f_6_ = lean_alloc_closure((void*)(lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Basic_Real_Basic_2153608522____hygCtx___hyg_2_), 3, 2);
+lean_closure_set(v___f_6_, 0, v_g_1_);
+lean_closure_set(v___f_6_, 1, v___f_5_);
+return v___f_6_;
+}
+}
+static lean_object* _init_lp_TemGeometryPaper_TEMPaper_nuEq22___closed__0(void){
+_start:
+{
+lean_object* v___x_7_; lean_object* v___x_8_; 
+v___x_7_ = lean_unsigned_to_nat(3u);
+v___x_8_ = lp_mathlib_Nat_cast___at___00Nat_cast___at___00Nat_cast___at___00Nat_cast___at___00__private_Mathlib_NumberTheory_ModularForms_EisensteinSeries_E2_Transform_0__EisensteinSeries_00_u03b4_spec__0_spec__0_spec__2_spec__3(v___x_7_);
+return v___x_8_;
+}
+}
+static lean_object* _init_lp_TemGeometryPaper_TEMPaper_nuEq22___closed__1(void){
+_start:
+{
+lean_object* v___x_9_; lean_object* v___f_10_; 
+v___x_9_ = lean_obj_once(&lp_TemGeometryPaper_TEMPaper_nuEq22___closed__0, &lp_TemGeometryPaper_TEMPaper_nuEq22___closed__0_once, _init_lp_TemGeometryPaper_TEMPaper_nuEq22___closed__0);
+v___f_10_ = lean_alloc_closure((void*)(lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Basic_Real_Basic_4282658333____hygCtx___hyg_2_), 2, 1);
+lean_closure_set(v___f_10_, 0, v___x_9_);
+return v___f_10_;
+}
+}
+LEAN_EXPORT lean_object* lp_TemGeometryPaper_TEMPaper_nuEq22(lean_object* v_Gamma_11_, lean_object* v_T_12_){
+_start:
+{
+lean_object* v___f_13_; lean_object* v___f_14_; lean_object* v___f_15_; lean_object* v___f_16_; 
+v___f_13_ = lean_obj_once(&lp_TemGeometryPaper_TEMPaper_nuEq22___closed__1, &lp_TemGeometryPaper_TEMPaper_nuEq22___closed__1_once, _init_lp_TemGeometryPaper_TEMPaper_nuEq22___closed__1);
+v___f_14_ = lean_alloc_closure((void*)(lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Basic_Real_Basic_1745762101____hygCtx___hyg_2_), 3, 2);
+lean_closure_set(v___f_14_, 0, v___f_13_);
+lean_closure_set(v___f_14_, 1, v_Gamma_11_);
+v___f_15_ = lean_alloc_closure((void*)(lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Basic_Real_Basic_4282658333____hygCtx___hyg_2_), 2, 1);
+lean_closure_set(v___f_15_, 0, v_T_12_);
+v___f_16_ = lean_alloc_closure((void*)(lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Basic_Real_Basic_2153608522____hygCtx___hyg_2_), 3, 2);
+lean_closure_set(v___f_16_, 0, v___f_14_);
+lean_closure_set(v___f_16_, 1, v___f_15_);
+return v___f_16_;
+}
+}
+LEAN_EXPORT lean_object* lp_TemGeometryPaper_TEMPaper_kappaEq23(lean_object* v_Gamma_17_){
+_start:
+{
+lean_inc(v_Gamma_17_);
+return v_Gamma_17_;
+}
+}
+LEAN_EXPORT lean_object* lp_TemGeometryPaper_TEMPaper_kappaEq23___boxed(lean_object* v_Gamma_18_){
+_start:
+{
+lean_object* v_res_19_; 
+v_res_19_ = lp_TemGeometryPaper_TEMPaper_kappaEq23(v_Gamma_18_);
+lean_dec(v_Gamma_18_);
+return v_res_19_;
+}
+}
+static lean_object* _init_lp_TemGeometryPaper_TEMPaper_fisher1(void){
+_start:
+{
+lean_object* v___x_20_; 
+v___x_20_ = lean_obj_once(&lp_TemGeometryPaper_TEMPaper_nuEq22___closed__0, &lp_TemGeometryPaper_TEMPaper_nuEq22___closed__0_once, _init_lp_TemGeometryPaper_TEMPaper_nuEq22___closed__0);
+return v___x_20_;
+}
+}
+static lean_object* _init_lp_TemGeometryPaper_TEMPaper_GammaNormal1___closed__0(void){
+_start:
+{
+lean_object* v___x_21_; lean_object* v___x_22_; 
+v___x_21_ = lean_unsigned_to_nat(10u);
+v___x_22_ = lp_mathlib_Nat_cast___at___00Nat_cast___at___00Nat_cast___at___00Nat_cast___at___00__private_Mathlib_NumberTheory_ModularForms_EisensteinSeries_E2_Transform_0__EisensteinSeries_00_u03b4_spec__0_spec__0_spec__2_spec__3(v___x_21_);
+return v___x_22_;
+}
+}
+static lean_object* _init_lp_TemGeometryPaper_TEMPaper_GammaNormal1___closed__1(void){
+_start:
+{
+lean_object* v___x_23_; lean_object* v___f_24_; 
+v___x_23_ = lean_obj_once(&lp_TemGeometryPaper_TEMPaper_GammaNormal1___closed__0, &lp_TemGeometryPaper_TEMPaper_GammaNormal1___closed__0_once, _init_lp_TemGeometryPaper_TEMPaper_GammaNormal1___closed__0);
+v___f_24_ = lean_alloc_closure((void*)(lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Basic_Real_Basic_4282658333____hygCtx___hyg_2_), 2, 1);
+lean_closure_set(v___f_24_, 0, v___x_23_);
+return v___f_24_;
+}
+}
+static lean_object* _init_lp_TemGeometryPaper_TEMPaper_GammaNormal1(void){
+_start:
+{
+lean_object* v___f_25_; 
+v___f_25_ = lean_obj_once(&lp_TemGeometryPaper_TEMPaper_GammaNormal1___closed__1, &lp_TemGeometryPaper_TEMPaper_GammaNormal1___closed__1_once, _init_lp_TemGeometryPaper_TEMPaper_GammaNormal1___closed__1);
+return v___f_25_;
+}
+}
+static lean_object* _init_lp_TemGeometryPaper_TEMPaper_TNormal1___closed__0(void){
+_start:
+{
+lean_object* v___x_26_; lean_object* v___x_27_; 
+v___x_26_ = lean_unsigned_to_nat(14u);
+v___x_27_ = lp_mathlib_Nat_cast___at___00Nat_cast___at___00Nat_cast___at___00Nat_cast___at___00__private_Mathlib_NumberTheory_ModularForms_EisensteinSeries_E2_Transform_0__EisensteinSeries_00_u03b4_spec__0_spec__0_spec__2_spec__3(v___x_26_);
+return v___x_27_;
+}
+}
+static lean_object* _init_lp_TemGeometryPaper_TEMPaper_TNormal1(void){
+_start:
+{
+lean_object* v___x_28_; 
+v___x_28_ = lean_obj_once(&lp_TemGeometryPaper_TEMPaper_TNormal1___closed__0, &lp_TemGeometryPaper_TEMPaper_TNormal1___closed__0_once, _init_lp_TemGeometryPaper_TEMPaper_TNormal1___closed__0);
+return v___x_28_;
+}
+}
+static lean_object* _init_lp_TemGeometryPaper___private_TemGeometryPaper_0__TEMPaper_gPart1___closed__0(void){
+_start:
+{
+lean_object* v___x_29_; lean_object* v___x_30_; 
+v___x_29_ = lean_unsigned_to_nat(2u);
+v___x_30_ = lp_mathlib_Nat_cast___at___00Nat_cast___at___00Nat_cast___at___00Nat_cast___at___00__private_Mathlib_NumberTheory_ModularForms_EisensteinSeries_E2_Transform_0__EisensteinSeries_00_u03b4_spec__0_spec__0_spec__2_spec__3(v___x_29_);
+return v___x_30_;
+}
+}
+static lean_object* _init_lp_TemGeometryPaper___private_TemGeometryPaper_0__TEMPaper_gPart1___closed__1(void){
+_start:
+{
+lean_object* v___x_31_; lean_object* v___x_32_; 
+v___x_31_ = lean_unsigned_to_nat(6u);
+v___x_32_ = lp_mathlib_Nat_cast___at___00Nat_cast___at___00Nat_cast___at___00Nat_cast___at___00__private_Mathlib_NumberTheory_ModularForms_EisensteinSeries_E2_Transform_0__EisensteinSeries_00_u03b4_spec__0_spec__0_spec__2_spec__3(v___x_31_);
+return v___x_32_;
+}
+}
+LEAN_EXPORT lean_object* lp_TemGeometryPaper___private_TemGeometryPaper_0__TEMPaper_gPart1(lean_object* v_u_33_, lean_object* v_v_34_){
+_start:
+{
+lean_object* v_fst_35_; lean_object* v_snd_36_; lean_object* v_fst_37_; lean_object* v_snd_38_; lean_object* v___f_39_; lean_object* v___x_40_; lean_object* v___f_41_; lean_object* v___f_42_; lean_object* v___f_43_; lean_object* v___f_44_; lean_object* v___f_45_; lean_object* v___f_46_; lean_object* v___x_47_; lean_object* v___f_48_; lean_object* v___f_49_; lean_object* v___f_50_; 
+v_fst_35_ = lean_ctor_get(v_u_33_, 0);
+lean_inc_n(v_fst_35_, 2);
+v_snd_36_ = lean_ctor_get(v_u_33_, 1);
+lean_inc_n(v_snd_36_, 2);
+lean_dec_ref(v_u_33_);
+v_fst_37_ = lean_ctor_get(v_v_34_, 0);
+lean_inc_n(v_fst_37_, 2);
+v_snd_38_ = lean_ctor_get(v_v_34_, 1);
+lean_inc_n(v_snd_38_, 2);
+lean_dec_ref(v_v_34_);
+v___f_39_ = lean_alloc_closure((void*)(lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Basic_Real_Basic_1745762101____hygCtx___hyg_2_), 3, 2);
+lean_closure_set(v___f_39_, 0, v_fst_35_);
+lean_closure_set(v___f_39_, 1, v_fst_37_);
+v___x_40_ = lean_obj_once(&lp_TemGeometryPaper___private_TemGeometryPaper_0__TEMPaper_gPart1___closed__0, &lp_TemGeometryPaper___private_TemGeometryPaper_0__TEMPaper_gPart1___closed__0_once, _init_lp_TemGeometryPaper___private_TemGeometryPaper_0__TEMPaper_gPart1___closed__0);
+v___f_41_ = lean_alloc_closure((void*)(lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Basic_Real_Basic_1745762101____hygCtx___hyg_2_), 3, 2);
+lean_closure_set(v___f_41_, 0, v___x_40_);
+lean_closure_set(v___f_41_, 1, v_fst_35_);
+v___f_42_ = lean_alloc_closure((void*)(lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Basic_Real_Basic_1745762101____hygCtx___hyg_2_), 3, 2);
+lean_closure_set(v___f_42_, 0, v___f_41_);
+lean_closure_set(v___f_42_, 1, v_snd_38_);
+v___f_43_ = lean_alloc_closure((void*)(lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Basic_Real_Basic_2153608522____hygCtx___hyg_2_), 3, 2);
+lean_closure_set(v___f_43_, 0, v___f_39_);
+lean_closure_set(v___f_43_, 1, v___f_42_);
+v___f_44_ = lean_alloc_closure((void*)(lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Basic_Real_Basic_1745762101____hygCtx___hyg_2_), 3, 2);
+lean_closure_set(v___f_44_, 0, v___x_40_);
+lean_closure_set(v___f_44_, 1, v_snd_36_);
+v___f_45_ = lean_alloc_closure((void*)(lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Basic_Real_Basic_1745762101____hygCtx___hyg_2_), 3, 2);
+lean_closure_set(v___f_45_, 0, v___f_44_);
+lean_closure_set(v___f_45_, 1, v_fst_37_);
+v___f_46_ = lean_alloc_closure((void*)(lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Basic_Real_Basic_2153608522____hygCtx___hyg_2_), 3, 2);
+lean_closure_set(v___f_46_, 0, v___f_43_);
+lean_closure_set(v___f_46_, 1, v___f_45_);
+v___x_47_ = lean_obj_once(&lp_TemGeometryPaper___private_TemGeometryPaper_0__TEMPaper_gPart1___closed__1, &lp_TemGeometryPaper___private_TemGeometryPaper_0__TEMPaper_gPart1___closed__1_once, _init_lp_TemGeometryPaper___private_TemGeometryPaper_0__TEMPaper_gPart1___closed__1);
+v___f_48_ = lean_alloc_closure((void*)(lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Basic_Real_Basic_1745762101____hygCtx___hyg_2_), 3, 2);
+lean_closure_set(v___f_48_, 0, v___x_47_);
+lean_closure_set(v___f_48_, 1, v_snd_36_);
+v___f_49_ = lean_alloc_closure((void*)(lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Basic_Real_Basic_1745762101____hygCtx___hyg_2_), 3, 2);
+lean_closure_set(v___f_49_, 0, v___f_48_);
+lean_closure_set(v___f_49_, 1, v_snd_38_);
+v___f_50_ = lean_alloc_closure((void*)(lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Basic_Real_Basic_2153608522____hygCtx___hyg_2_), 3, 2);
+lean_closure_set(v___f_50_, 0, v___f_46_);
+lean_closure_set(v___f_50_, 1, v___f_49_);
+return v___f_50_;
+}
+}
+lean_object* initialize_Init(uint8_t builtin);
+lean_object* initialize_Init(uint8_t builtin);
+lean_object* initialize_mathlib_Mathlib(uint8_t builtin);
+void lean_initialize();
+static bool _G_initialized = false;
+LEAN_EXPORT lean_object* initialize_TemGeometryPaper_TemGeometryPaper(uint8_t builtin) {
+lean_object * res;
+if (_G_initialized) return lean_io_result_mk_ok(lean_box(0));
+_G_initialized = true;
+lean_initialize();
+res = initialize_Init(builtin);
+if (lean_io_result_is_error(res)) return res;
+lean_dec_ref(res);
+res = initialize_Init(builtin);
+if (lean_io_result_is_error(res)) return res;
+lean_dec_ref(res);
+res = initialize_mathlib_Mathlib(builtin);
+if (lean_io_result_is_error(res)) return res;
+lean_dec_ref(res);
+lp_TemGeometryPaper_TEMPaper_fisher1 = _init_lp_TemGeometryPaper_TEMPaper_fisher1();
+lean_mark_persistent(lp_TemGeometryPaper_TEMPaper_fisher1);
+lp_TemGeometryPaper_TEMPaper_GammaNormal1 = _init_lp_TemGeometryPaper_TEMPaper_GammaNormal1();
+lean_mark_persistent(lp_TemGeometryPaper_TEMPaper_GammaNormal1);
+lp_TemGeometryPaper_TEMPaper_TNormal1 = _init_lp_TemGeometryPaper_TEMPaper_TNormal1();
+lean_mark_persistent(lp_TemGeometryPaper_TEMPaper_TNormal1);
+return lean_io_result_mk_ok(lean_box(0));
+}
+#ifdef __cplusplus
+}
+#endif
